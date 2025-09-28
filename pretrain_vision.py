2f469a5ba6f409dbfe2fa07aa701bfc1855ef555
Aditya Kar <aditya.kar@students.iiserpune.ac.in>
2025-09-28 14:38:44 +0200

    LR issue fixed

--- pretrain_vision.py
+++ pretrain_vision.py
@@ -201,13 +201,13 @@ def create_model(config: PretrainVisionConfig, train_metadata: CIFARDatasetMetad
     return model, optimizers, optimizer_lrs
 
 def cosine_schedule_with_warmup_lr_lambda(
-    current_step: int, *, base_lr: float, num_warmup_steps: int,
+    current_step: int, *, num_warmup_steps: int,
     num_training_steps: int, min_ratio: float = 0.0, num_cycles: float = 0.5
 ):
     if current_step < num_warmup_steps:
         return float(current_step) / float(max(1, num_warmup_steps))
     progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
-    return base_lr * (min_ratio + max(0.0, (1 - min_ratio) * 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress))))
+    return min_ratio + max(0.0, (1 - min_ratio) * 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
 
 def init_train_state(config: PretrainVisionConfig, train_metadata: CIFARDatasetMetadata, world_size: int):
     
@@ -265,10 +265,11 @@ def train_batch(train_state: TrainState, batch: tuple, config: PretrainVisionCon
 
     lr_this_step = None
     for optim, base_lr in zip(train_state.optimizers, train_state.optimizer_lrs):
-        lr_this_step = cosine_schedule_with_warmup_lr_lambda(
-            current_step=train_state.step, base_lr=base_lr, num_warmup_steps=config.lr_warmup_steps,
+        lr_multiplier = cosine_schedule_with_warmup_lr_lambda(
+            current_step=train_state.step, num_warmup_steps=config.lr_warmup_steps,
             num_training_steps=train_state.total_steps, min_ratio=config.lr_min_ratio
         )
+        lr_this_step = base_lr * lr_multiplier
         for param_group in optim.param_groups:
             param_group['lr'] = lr_this_step
 
